8de4e3ad0c42b05e3809e8e78cea87a35be061dc
Jim29800 <jbouvier1173@gmail.com>
2017-07-26 14:17:09 +0200

    Ajout des fonctions reload on logo, alert links, text photos

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-$("input").click(function(){
-	var imageSelect = $(this).data("tech")
-	console.log(imageSelect)
+$(".logo").click(function(){
+	location.reload();	
+});
 
 
-});;
+$("#link1").click(function(){
+	alert("Execute Order 66")	
+});
+
+$("#link2").click(function(){
+	alert("Je viens du nord et je suis l'empereur Palpainauchocolat")	
+});
+
+$("DIV.images input").click(function(){
+	console.log("PAIN AU CHOCOLAT BORDEL")
+});
